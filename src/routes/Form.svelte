20fa08c895a54aa32e44fd5fcba5c8829c74823b
Your Name≈≈ <shantanu@thecoderpanda.com>
2024-01-09 14:38:05 +0530

    Initial Design

---
+++ src/routes/Form.svelte
@@ -0,0 +1,14 @@
+<script>
+	function validate() {
+		console.log("I'm the validate() function")
+	}
+</script>
+
+<form on:submit|preventDefault={validate}>
+    <input type="text" placeholder="Enter your Wallet Address">
+    <input type="text" placeholder="Enter your Email">
+
+	<button type="submit">
+		Subscribe
+	</button>
+</form>
